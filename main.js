bad343b757136075d0025ee6b3fd4f9cbc761e24
Ali ABBAS <ali.ibra.abbas@gmail.com>
2023-02-17 15:52:34 +0100

    Add center section + graph

---
+++ main.js
@@ -0,0 +1,29 @@
+// let graph = document.getElementById('graph').getContext('2d')
+
+// new CharacterData(graph, {
+//     type: 'line',
+//     data: {
+//         labels: [
+//             '5Nov',
+//             '6Nov',
+//             '7Nov',
+//             '8Nov',
+//             '9Nov',
+//             '10Nov',
+//             '11Nov',
+//             '12Nov',
+//             '13Nov',
+//             '14Nov',
+//             '15Nov',
+//         ],
+//         datasets: [
+//             {
+//                 label: 'ETH',
+//                 data:[100, 400, 600, 400, 300, 100, 350, 200, 300, 700, 600],
+//                 borderColor: 'rgba(255, 99, 132, 1)',
+//                 borderColor: transparent,
+
+//             }
+//         ]
+//     }
+// })
